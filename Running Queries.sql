275079c073fc134e4cfc09c53a435f3f4889bc9f
monkeycoward <85740394+monkeycoward@users.noreply.github.com>
2024-02-13 16:49:34 +0100

    Update Running Queries.sql

--- Running Queries.sql
+++ Running Queries.sql
@@ -1,16 +1,14 @@
 SELECT
-  session_id as SPID,
+  r.session_id as SPID,
   CONVERT(VARCHAR(50),start_time,100) AS start_time,
   percent_complete,
   CONVERT(VARCHAR(50),dateadd(second,estimated_completion_time/1000, getdate()),100) as estimated_completion_time,
-  command, a.text AS Query,
-  blocking_session_id
+  command, 
+  a.text AS Query,
+  blocking_session_id,
+  d.name, 
+  u.login_name
 FROM sys.dm_exec_requests r
 CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) a
-
----
-select r.session_id, start_time, command, a.text, blocking_session_id, d.name, u.login_name
-from sys.dm_exec_requests r
-cross apply sys.dm_exec_sql_text (r.sql_handle) a
-inner join sys.databases d on r.database_id = d.database_id
-inner join sys.dm_exec_sessions u on r.session_id = u.session_id
+INNER JOIN sys.databases d on r.database_id = d.database_id
+INNER JOIN sys.dm_exec_sessions u on r.session_id = u.session_id
